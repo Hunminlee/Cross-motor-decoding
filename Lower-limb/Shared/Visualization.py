78e3d7f3a41793266cad8ec36d603b1e97c0064a
Hunminlee <hml763@naver.com>
2025-08-19 15:14:23 -0500

    Gait1-v1

--- Lower-limb/Shared/Visualization.py
+++ Lower-limb/Shared/Visualization.py
@@ -4,4 +4,5 @@ def learning_plot(history):
     plt.plot(history.history['accuracy'], label='train acc')
     plt.plot(history.history['val_accuracy'], label='val acc')
     plt.legend()
+    plt.grid(True, linestyle='--', c='grey', which='major', pad=0.1)
     plt.show()
